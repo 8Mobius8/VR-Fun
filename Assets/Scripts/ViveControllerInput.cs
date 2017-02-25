95978e651983fab4842ba61ab0a8d4c4144af431
Mark Odell <dracone.cordis@gmail.com>
2017-02-24 22:43:22 -0800

    Grab objects.

--- Assets/Scripts/ViveControllerInput.cs
+++ Assets/Scripts/ViveControllerInput.cs
@@ -13,29 +13,29 @@ public class ViveControllerInput : MonoBehaviour {
 		trackedObj = GetComponent<SteamVR_TrackedObject>();
 	}
 	void Update () {
-		if (Controller.GetAxis() != Vector2.zero)
-		{
-			Debug.Log(gameObject.name + Controller.GetAxis());
-		}
+		// if (Controller.GetAxis() != Vector2.zero)
+		// {
+		// 	Debug.Log(gameObject.name + Controller.GetAxis());
+		// }
 		
-		if (Controller.GetHairTriggerDown())
-		{
-			Debug.Log(gameObject.name + " Trigger Press");
-		}
+		// if (Controller.GetHairTriggerDown())
+		// {
+		// 	Debug.Log(gameObject.name + " Trigger Press");
+		// }
 		
-		if (Controller.GetHairTriggerUp())
-		{
-			Debug.Log(gameObject.name + " Trigger Release");
-		}
+		// if (Controller.GetHairTriggerUp())
+		// {
+		// 	Debug.Log(gameObject.name + " Trigger Release");
+		// }
 		
-		if (Controller.GetPressDown(SteamVR_Controller.ButtonMask.Grip))
-		{
-			Debug.Log(gameObject.name + " Grip Press");
-		}
+		// if (Controller.GetPressDown(SteamVR_Controller.ButtonMask.Grip))
+		// {
+		// 	Debug.Log(gameObject.name + " Grip Press");
+		// }
 		
-		if (Controller.GetPressUp(SteamVR_Controller.ButtonMask.Grip))
-		{
-			Debug.Log(gameObject.name + " Grip Release");
-		}
+		// if (Controller.GetPressUp(SteamVR_Controller.ButtonMask.Grip))
+		// {
+		// 	Debug.Log(gameObject.name + " Grip Release");
+		// }
 	}
 }
